65064c2967da6c9e2cca44b51759922afdf0cb75
Angie Gomez <angie@Angies-MacBook-Air.local>
2019-06-12 16:51:15 -0700

    pypoll file update:more efficient

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -12,61 +12,49 @@ def outputPrint(text):
 
 #Rounds to the third decimal place
 def perc(votes):
-        perc = (votes/len(all_votes)) * 100
+        perc = (votes/counter) * 100
         return("%0.3f" % (perc))
 
 with open(csvpath, newline='') as csvfile:
     
+    #List for all candidates
     candidates = []
-    all_votes = []
-    sum0 = 0
-    sum1 = 0
-    sum2 = 0
-    sum3 = 0
-    starter = 0
+    #Dictionary for each candidate's vote count
+    votes = {}
+    #Initiate the vote counter
+    counter = 0
 
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    #Skip the header
     csv_header = next(csvreader)
 
     for row in csvreader:
-        #Add all votes to a list
-        all_votes.append(row[2])
-        #If the current candidate is not already in the candidates list, add it
-        #This list holds all candidates that are running
-        if row[2] not in candidates:
-            candidates.append(row[2])
 
-    #If any of the votes match one of the candidates in the list, increase candidate's votes by 1      
-    for x in range(len(all_votes)):
-        if all_votes[x] == candidates[0]:
-            sum0 += 1
-        elif all_votes[x] == candidates[1]:
-            sum1 += 1
-        elif all_votes[x] == candidates[2]:
-            sum2 += 1
-        elif all_votes[x] == candidates[3]:
-            sum3 += 1
+        #Add on to the vote counter
+        counter += 1
+        #Candidate's name
+        candidate = row[2]
+
+        #If the candidate is not in the current candidate's list...
+        if candidate not in candidates:
+            #Add it to the list
+            candidates.append(candidate)
+            #Add it to the dictionary and set the vote count to one
+            votes[candidate] = 1
         else:
-            pass
+            #Add one to the candidate's current vote count
+            votes[candidate] +=1
 
-    #Add each candidate's sum of votes to a list
-    votes_list = [sum0, sum1, sum2, sum3]
-    for x in range(len(votes_list)):
-        #If the current candidate's sum of votes is higher then the previous, that candidate becomes the winner
-        if votes_list[x] > starter:
-            winner = candidates[x]
-            starter = votes_list[x]
+    #Find the candidate with the highest vote count
+    winner = max(votes, key=votes.get)
 
     #Print to console and text file in an efficient way
     outputPrint("Election Results")
     outputPrint("-------------------------")
-    outputPrint(f"Total Votes: {len(all_votes)}")
+    outputPrint(f"Total Votes: {counter}")
     outputPrint("-------------------------")
-    outputPrint(f"{candidates[0]}: {perc(sum0)}% ({sum0})")
-    outputPrint(f"{candidates[1]}: {perc(sum1)}% ({sum1})")
-    outputPrint(f"{candidates[2]}: {perc(sum2)}% ({sum2})")
-    outputPrint(f"{candidates[3]}: {perc(sum3)}% ({sum3})")
+    for candidate in candidates:
+        outputPrint(f"{candidate}: {perc(votes[candidate])}% ({votes[candidate]})")
     outputPrint("-------------------------")
     outputPrint(f"Winner: {winner}")
     outputPrint("-------------------------")
